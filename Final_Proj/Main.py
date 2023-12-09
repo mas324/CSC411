053d25f7620fce4288f88dd3085153424567ba08
Shawn <shawnmerana@gmail.com>
2023-12-08 17:24:50 -0800

    Basic outline of the program

--- Final_Proj/Main.py
+++ Final_Proj/Main.py
@@ -1,5 +1,6 @@
 
 from cProfile import label
+import os
 import re
 from matplotlib import pyplot as plt
 import numpy as np
@@ -8,30 +9,47 @@ np.random.seed(13)
 LEARNING_RATE = 0.01
 EPOCH = 5
 
-input_neurons = 64 * 64
-hidden_neurons = 16
-output_neurons = 8
-MASTER_FILE = 'Final_Proj/chrome.out'
+input_neurons_count = 64 * 64
+hidden_neurons_count = 16
+output_neurons_count = 8
+DATA_FILE = 'chrome.out'
+LOCAL = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def read_dataset():
     #Read in as a 2D array 1048576 x 7
-    file_read = np.loadtxt(MASTER_FILE, dtype={'names':('theta_i', 'phi_i', 'theta_o', 'phi_o', 'red', 'green', 'blue'),
-                                               'formats':('f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4')})
+    #File must be in format f, f, f, f, f, f, f
+    #Will only need first 5 for this assignment
+    file_read = np.loadtxt(os.path.join(LOCAL, DATA_FILE), np.dtypes.Float16DType)
+    
+    #Split data, half to train and half to test
+    data_split = np.split(file_read, 2)
+    x_train = data_split[0]
+    x_test = data_split[1]
 
-    x_train = np.split(file_read, 2)[0]
-    x_test = file_read
-    y_train = np.zeros(len(file_read / 2))
-    y_test = np.zeros(len(file_read))
+    #One-hot encode
+    y_train = np.zeros((len(x_train), output_neurons_count))
+    y_test = np.zeros((len(x_test), output_neurons_count))
+    y_train[np.random.randint(len(y_train))][np.random.randint(output_neurons_count)] = 1
+    y_test[np.random.randint(len(y_test))][np.random.randint(output_neurons_count)] = 1
     return x_train, x_test, y_train, y_test
     
 x_train, y_train, x_test, y_test = read_dataset()
 index_list = list(range(len(x_train)))
 
-def neuron_w(input_count):
-    weights = np.zeros(input_count + 1)
-    for i in range(1, (input_count + 1)):
-        weights[i] = np.random.uniform(-1.0, 1.0)
+def neuron_w(neuron_count, input_count):
+    weights = np.zeros((neuron_count, input_count + 1))
+    for n in range(neuron_count):
+        for i in range(1, (input_count + 1)):
+            weights[n][i] = np.random.uniform(-1.0, 1.0)
     return weights
+
+hidden_weight = neuron_w(hidden_neurons_count, input_neurons_count)
+hidden_layer_y = np.zeros(hidden_neurons_count)
+hidden_error = np.zeros(hidden_neurons_count)
+
+output_weight = neuron_w(output_neurons_count, hidden_neurons_count)
+output_layer_y = np.zeros(output_neurons_count)
+output_error = np.zeros(output_neurons_count)
 
 chart_x = []
 chart_y_train = []
@@ -46,7 +64,7 @@ def show_learning(epoch, train, test):
 def plot_learning():
     plt.plot(chart_x, chart_y_train, 'r-', label='training error')
     plt.plot(chart_x, chart_y_test, 'b-', label='test error')
-    plt.axis([0, len(chart_x), 0.0, 1.0])
+    plt.axis((0, len(chart_x), 0.0, 1.0))
     plt.xlabel('training epochs')
     plt.ylabel('error')
     plt.legend()
@@ -54,4 +72,54 @@ def plot_learning():
 
     
 def forward_pass(x):
-    return
+    global hidden_layer_y, output_layer_y
+    for i, w in enumerate(hidden_weight):
+        z = np.dot(w, x)
+        hidden_layer_y[i] = np.tanh(z)
+    hidden_output = np.concatenate((np.array([1.0]), hidden_layer_y))
+    for i, w in enumerate(output_weight):
+        z = np.dot(w, hidden_output)
+        output_layer_y[i] = 1.0 / (1.0 + np.exp(-z))
+        
+def backward_pass(y_truth):
+    global hidden_error, output_error
+    for i, y in enumerate(output_layer_y):
+        error_p = -(y_truth[i] - y)
+        derive = y * (1.0 - y)
+        output_error[i] = error_p * derive
+    for i, y in enumerate(hidden_layer_y):
+        error_w = []
+        for w in output_weight:
+            error_w.append(w[i+1])
+        error_w_array = np.array(error_w)
+        derive = 1.0 - y**2
+        weight_error = np.dot(error_w_array, output_error)
+        hidden_error[i] = weight_error * derive
+    
+def adjust_weights(x):
+    global output_weight, hidden_weight
+    for i, err in enumerate(hidden_error):
+        hidden_weight[i] -= (x * LEARNING_RATE * err)
+    hidden_array = np.concatenate((np.array([1.0]), hidden_layer_y))
+    for i, err in enumerate(output_error):
+        output_weight[i] -= (hidden_array * LEARNING_RATE * err)
+        
+#Start training and testing
+for i in range(EPOCH):
+    np.random.shuffle(index_list)
+    correct_training = 0
+    for j in index_list:
+        x = np.concatenate((np.array([1.0]), x_train[j]))
+        forward_pass(x)
+        if output_layer_y.argmax() == y_train[j].argmax():
+            correct_training += 1
+        backward_pass(y_train[j])
+        adjust_weights(x)
+    correct_testing = 0
+    for j in range(len(x_test)):
+        x = np.concatenate((np.array([1.0]), x_test[j]))
+        forward_pass(x)
+        if output_layer_y.argmax() == y_test[j].argmax():
+            correct_testing += 1
+    show_learning(i, correct_training/len(x_train), correct_testing/len(x_test))
+plot_learning()
